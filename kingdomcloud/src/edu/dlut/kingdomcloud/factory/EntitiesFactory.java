40b14f3cd9da91eb70a686c6c398683deb061620
hitmit1314 <hitmit1314@8269a6a6-f441-5c48-cdfd-2a84f49edc5e>
2010-06-25 18:20:46 +0000

    Cartoon

--- kingdomcloud/src/edu/dlut/kingdomcloud/factory/EntitiesFactory.java
+++ kingdomcloud/src/edu/dlut/kingdomcloud/factory/EntitiesFactory.java
@@ -1,5 +1,9 @@
 package edu.dlut.kingdomcloud.factory;
 
-public class EntitiesFactory {
+import edu.dlut.kingdomcloud.entity.AccountEntity;
 
+public class EntitiesFactory {
+	public static AccountEntity productAccountEntity()throws Exception{
+		return new AccountEntity();
+	}
 }
